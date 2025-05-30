a3b7596935f3f464c389330002a7f3745cc0c73c
AbdullohAA <abdullohabduganiev55555@gmail.com>
2025-05-30 09:20:25 +0300

    changed name to sales

--- src/app/sales/page.tsx
+++ src/app/sales/page.tsx
@@ -19,7 +19,7 @@ export default function Sales() {
     <div className="container mx-auto mb-20">
       <Breadcrumbs items={scrums} />
       <div>
-        <h2 className="text-center text-3xl mb-5">Блог</h2>
+        <h2 className="text-center text-3xl mb-5">Акции</h2>
         <div className="grid grid-cols-2  gap-2.5 md:gap-5">
           {[...Array(7)].map((_, i) => (
             <Link key={i} href={"/sales/1"} className=" bg-[#161616]  relative">
