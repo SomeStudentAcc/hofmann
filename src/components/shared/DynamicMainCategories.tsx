05227818ad5331bd8a50de5d7d744215cbe7af0d
AbdullohAA <abdullohabduganiev55555@gmail.com>
2025-06-03 11:02:12 +0300

    added lazy

--- src/components/shared/DynamicMainCategories.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from "next/dynamic";
-
-const MainCategorySingle = dynamic(() => import("./MainCategorySingle"), {
-  loading: () => <div>Loading...</div>, // optional fallback
-  ssr: false, // optional: disables SSR for this component
-});
